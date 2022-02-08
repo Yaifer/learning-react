2cbce06ce062ae5e7f96155a8851a3943486b05e
Yaifer <JHANNON@madisoncollege.edu>
2022-02-07 23:33:00 -0600

    Destructuring props
    Props destructing

--- learning-react/src/Greeting.js
+++ learning-react/src/Greeting.js
@@ -1,19 +1,23 @@
-const Greeting = (props) => {
+const Greeting = ({
+    name,
+    favoriteNumber,
+    favoriteColors
+}) => {
 
-console.log(props)
+
 
     return (
         <>
-            <h2>Hello {props.name}!!!</h2>
-            <h3>{props.name}'s favorite number is {props.favoriteNumber}</h3>
-            <h3>{props.name}'s favorite number + 10 is {props.favoriteNumber + 10}</h3>
-            <h3>{props.name}'s favorite colors are:</h3>
+            <h2>Hello {name}!!!</h2>
+            <h3>{name}'s favorite number is {favoriteNumber}</h3>
+            <h3>{name}'s favorite number + 10 is {favoriteNumber + 10}</h3>
+            <h3>{name}'s favorite colors are:</h3>
             <ul>
-                {props.favoriteColors && [...new Set(props.favoriteColors)].map(x => <li key={x}>{x}</li>)}
-                {!props.favoriteColors && <li>None</li>}
+                {favoriteColors && [...new Set(favoriteColors)].map(x => <li key={x}>{x}</li>)}
+                {!favoriteColors && <li>None</li>}
             </ul>
         </>       
     );
   };
-
+ 
   export default Greeting;
